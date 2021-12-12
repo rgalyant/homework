94f335481a6ead15a1932c5e09689aa7412bd290
Rustam <galyant20@gmail.com>
2021-12-12 09:48:56 +0200

    First commit added .gitignore added README.md

--- lesson_6_1/task_1.py
+++ lesson_6_1/task_1.py
@@ -1,11 +1,14 @@
 from random import randint
 
-L = [randint(0, 10) for _ in range(20)]
-k = randint(0, len(L) - 1)
+l = [randint(0, 10) for _ in range(20)]
+k = randint(0, len(l) - 1)
 
 print('K:', k)
-print(f'List (before): <id: {id(L)}> {L}')
+print(f'List (before): <id: {id(l)}> {l}')
 
-L[k:] = L[k:][1:]
+if 0 <= k < len(l):
+    for i in range(k + 1, len(l)):
+        l[i - 1] = l[i]
+    l.pop()
 
-print(f'List (after) : <id: {id(L)}> {L}')
+print(f'List (after) : <id: {id(l)}> {l}')
--- lesson_6_1/task_3.py
+++ lesson_6_1/task_3.py
@@ -6,6 +6,6 @@ l2 = [randint(0, 10) for _ in range(20)]
 print('List 1:', l1)
 print('List 2:', l2)
 
-c = len(set([n if l1.count(n) == 1 else None for n in l1] + [None]).symmetric_difference(set([n if l2.count(n) == 1 else None for n in l2] + [None])))
+c = sum([(l1 + l2).count(n) == 1 for n in (l1 + l2)])
 
 print('Uniques:', c)
